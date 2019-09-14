ba4c1c45a122c49676508f9db2bcb1750503f7a6
Digby Knight <Digby.Knight@Sophos.com>
2019-09-14 17:11:41 +0100

    Change

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", name=os.getenv('TEST'))
+    return render_template("index.html", name=os.getenv('TESTss'))
 
 
 if __name__ == "__main__":
