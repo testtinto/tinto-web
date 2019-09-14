e82e9a97a17fdf1ab385a7eab3a5aadd2634453f
Digby Knight <Digby.Knight@Sophos.com>
2019-09-14 17:01:20 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, url_for
+from datetime import datetime
+import re
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=os.getenv('TEST'))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
