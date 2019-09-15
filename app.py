18fe8330720c1d7cb2f60ab4789678eae4901fc0
Digby Knight <Digby.Knight@Sophos.com>
2019-09-15 20:07:48 +0100

    Stuff

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, jsonify
 from datetime import datetime
 import re
 import os
@@ -14,6 +14,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html", name=os.getenv('TEST'))
+
+
+@app.route("/user", methods=['GET'])
+def user():
+    return {"email": 'jow@bloggs.com', "age": 23}
 
 
 if __name__ == "__main__":
