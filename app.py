0d4e86ffda11a36c55cdaa4731ae96cdd4be6692
Digby Knight <digby@digby.net>
2019-09-20 18:33:33 +0100

    Verious changes

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-
+load_dotenv()
 app = Flask(__name__)
 
 
