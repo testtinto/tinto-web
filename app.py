15e12bb75b41c286e52b5b37dc9ee0430734284f
Digby Knight <digby@digby.net>
2019-09-20 18:39:18 +0100

    Auth0 changes

--- app.py
+++ app.py
@@ -1,23 +1,108 @@
-from flask import Flask, render_template, url_for, jsonify
-from datetime import datetime
-import re
+from functools import wraps
+import json
 import os
-from os.path import join, dirname
+from werkzeug.exceptions import HTTPException
+
 from dotenv import load_dotenv
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
+
+import constants
 
 load_dotenv()
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/public', static_folder='./public')
+app.secret_key = constants.SECRET_KEY
+app.debug = True
+
+AUTH0_CALLBACK_URL = os.getenv(constants.AUTH0_CALLBACK_URL)
+AUTH0_CLIENT_ID = os.getenv(constants.AUTH0_CLIENT_ID)
+AUTH0_CLIENT_SECRET = os.getenv(constants.AUTH0_CLIENT_SECRET)
+AUTH0_DOMAIN = os.getenv(constants.AUTH0_DOMAIN)
+AUTH0_BASE_URL = 'https://' + AUTH0_DOMAIN
+AUTH0_AUDIENCE = os.getenv(constants.AUTH0_AUDIENCE)
+if AUTH0_AUDIENCE is '':
+    AUTH0_AUDIENCE = AUTH0_BASE_URL + '/userinfo'
 
 
-@app.route("/")
-def index():
-    return render_template("index.html", name=os.getenv('TEST'))
+@app.errorhandler(Exception)
+def handle_auth_error(ex):
+    response = jsonify(message=str(ex))
+    response.status_code = (ex.code if isinstance(ex, HTTPException) else 500)
+    return response
 
 
-@app.route("/user", methods=['GET'])
-def user():
-    return {"email": 'jow@bloggs.com', "age": 23}
+oauth = OAuth(app)
 
+auth0 = oauth.register(
+    'auth0',
+    client_id=AUTH0_CLIENT_ID,
+    client_secret=AUTH0_CLIENT_SECRET,
+    api_base_url=AUTH0_BASE_URL,
+    access_token_url=AUTH0_BASE_URL + '/oauth/token',
+    authorize_url=AUTH0_BASE_URL + '/authorize',
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+)
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if constants.PROFILE_KEY not in session:
+            return redirect('/login')
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+# Controllers API
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/callback')
+def callback_handling():
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
+
+    session[constants.JWT_PAYLOAD] = userinfo
+    session[constants.PROFILE_KEY] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/dashboard')
+
+
+@app.route('/login')
+def login():
+    return auth0.authorize_redirect(redirect_uri=AUTH0_CALLBACK_URL, audience=AUTH0_AUDIENCE)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    params = {'returnTo': url_for(
+        'home', _external=True), 'client_id': AUTH0_CLIENT_ID}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
+
+
+@app.route('/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session[constants.PROFILE_KEY],
+                           userinfo_pretty=json.dumps(session[constants.JWT_PAYLOAD], indent=4))
 
 if __name__ == "__main__":
     app.run(debug=True)
